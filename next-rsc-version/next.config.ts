09e3d5cc1df101ee754fde5991efa022c7e0058f
Theo Browne <me@t3.gg>
2024-10-26 21:46:31 -0700

    next version rough example

--- next-rsc-version/next.config.ts
+++ next-rsc-version/next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    ppr: true,
+    dynamicIO: true,
+    cacheLife: {
+      default: {},
+      forever: {
+        expire: 999999999,
+        revalidate: 999999999,
+      },
+    },
+  },
 };
 
 export default nextConfig;
---
+++ next-rsc-version/src/sdk/pokeapi.ts
@@ -0,0 +1,50 @@
+import { unstable_cacheLife } from "next/cache";
+
+export async function getAllPokemon() {
+  "use cache";
+  unstable_cacheLife("forever");
+
+  const query = `
+    query GetAllPokemon {
+      pokemon_v2_pokemon {
+        id
+        pokemon_v2_pokemonspecy {
+          name
+        }
+      }
+    }
+  `;
+
+  const response = await fetch("https://beta.pokeapi.co/graphql/v1beta", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+  });
+
+  const data = (await response.json()).data as {
+    pokemon_v2_pokemon: {
+      id: number;
+      pokemon_v2_pokemonspecy: {
+        name: string;
+      };
+    }[];
+  };
+
+  return data.pokemon_v2_pokemon.map((pokemon) => ({
+    name: pokemon.pokemon_v2_pokemonspecy.name,
+    dexNumber: pokemon.id,
+    sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`,
+  }));
+}
+
+import { connection } from "next/server";
+
+export async function getTwoRandomPokemon() {
+  await connection(); // Next needed some help knowing this is dynamic
+
+  const allPokemon = await getAllPokemon();
+  const shuffled = allPokemon.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, 2);
+}
