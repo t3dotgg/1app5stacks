26bb06173433f9f4c17091a929cb9ef33ade3aed
Theo Browne <me@t3.gg>
2024-11-06 23:27:24 -0800

    use vite plugin for codegen

--- go-graphql-spa-version/react-gql-spa/vite.config.ts
+++ go-graphql-spa-version/react-gql-spa/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import codegen from "vite-plugin-graphql-codegen";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), codegen()],
+});
