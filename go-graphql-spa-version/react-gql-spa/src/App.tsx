bb2980e03424f4981df98d5370b15ef40df5a8c8
Theo Browne <me@t3.gg>
2024-11-07 01:17:11 -0800

    tailwind working

--- go-graphql-spa-version/react-gql-spa/src/App.tsx
+++ go-graphql-spa-version/react-gql-spa/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { useQuery } from "@apollo/client";
 
 import { gql } from "../__generated__/";
@@ -21,7 +20,7 @@ function App() {
 
   console.log("data", data);
 
-  return <div>Hello</div>;
+  return <div className="text-3xl font-bold">Hello</div>;
 }
 
 export default App;
