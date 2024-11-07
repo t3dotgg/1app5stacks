511aa8193c9d469eb611307a3fd6abf9fc901d29
Theo Browne <me@t3.gg>
2024-11-06 22:58:18 -0800

    lmao it already works

--- go-graphql-spa-version/react-gql-spa/src/graphql-client.ts
+++ go-graphql-spa-version/react-gql-spa/src/graphql-client.ts
@@ -1,6 +1,21 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
 
 export const graphqlClient = new ApolloClient({
-  uri: "https://flyby-router-demo.herokuapp.com/",
+  uri: "https://roundest-go.fly.dev/graphql",
   cache: new InMemoryCache(),
 });
+
+export const getPokemon = async () => {
+  return await graphqlClient.query({
+    query: gql`
+      query Pokemon {
+        pokemon {
+          name
+          id
+          upVotes
+          downVotes
+        }
+      }
+    `,
+  });
+};
