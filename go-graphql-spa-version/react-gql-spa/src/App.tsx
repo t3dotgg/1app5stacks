14275f964ac39213195267610078bf4181253693
Theo Browne <me@t3.gg>
2024-11-06 23:20:32 -0800

    Real queries with working typegen

--- go-graphql-spa-version/react-gql-spa/src/App.tsx
+++ go-graphql-spa-version/react-gql-spa/src/App.tsx
@@ -1,41 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { getPokemon } from "./graphql-client";
+import { useQuery } from "@apollo/client";
+
+import { gql } from "./__generated__/gql";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={async () => {
-            const pokemon = await getPokemon();
-            console.log(pokemon);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const { data, loading } = useQuery(
+    gql(`
+    query RandomPair {
+      randomPair {
+        pokemonOne {
+          id
+          name
+        }
+        pokemonTwo {
+          id
+          name
+        }
+      }
+    }
+  `)
   );
+
+  if (loading || !data) return <div>Loading...</div>;
+
+  console.log("data", data);
+
+  return <div>Hello</div>;
 }
 
 export default App;
