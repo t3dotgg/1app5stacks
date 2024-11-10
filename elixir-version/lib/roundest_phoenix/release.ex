7a3f02795fc40a02230dec85184eacba47441d8f
Theo Browne <me@t3.gg>
2024-11-09 19:07:06 -0800

    More cleanup

--- elixir-version/lib/roundest_phoenix/release.ex
+++ elixir-version/lib/roundest_phoenix/release.ex
@@ -13,30 +13,6 @@ defmodule RoundestPhoenix.Release do
     end
   end
 
-  defp priv_path_for(repo, filename) do
-    app = Keyword.get(repo.config(), :otp_app)
-    priv_dir = "#{:code.priv_dir(app)}"
-    Path.join([priv_dir, "repo", filename])
-  end
-
-  def seed do
-    load_app()
-
-    for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &run_seeds_for/1)
-    end
-  end
-
-  defp run_seeds_for(repo) do
-    # Run the seed script if it exists
-    seed_script = priv_path_for(repo, "seeds.exs")
-
-    if File.exists?(seed_script) do
-      IO.puts("Running seeds for #{repo}...")
-      Code.eval_file(seed_script)
-    end
-  end
-
   def rollback(repo, version) do
     load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
