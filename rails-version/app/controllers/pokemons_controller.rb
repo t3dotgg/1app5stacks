505bcfd64ef316cbbec4028c19095852fd0b3f73
EZ <testrailsemail@gmail.com>
2024-11-16 01:48:39 +0200

    [rails-version] lean out unused generated stuff
    (cherry picked from commit 7c97afe154fa11177a2483e5cef363b204491458)

--- rails-version/app/controllers/pokemons_controller.rb
+++ rails-version/app/controllers/pokemons_controller.rb
@@ -1,7 +1,4 @@
 class PokemonsController < ApplicationController
-  before_action :set_pokemon, only: %i[ show edit update destroy ]
-
-  # GET /pokemons or /pokemons.json
   def index
     cache_key = Pokemon.all.cache_key_with_version
     @pokemons = Rails.cache.fetch(cache_key, expires_in: 1.day) do
@@ -11,31 +8,6 @@ class PokemonsController < ApplicationController
     timestamp = cache_key.split('-').last
     last_modified_time = Time.strptime(timestamp, "%Y%m%d%H%M%S")
     fresh_when last_modified: last_modified_time, strong_etag: @pokemons
-  end
-
-  # GET /pokemons/1 or /pokemons/1.json
-  def show
-  end
-
-  # GET /pokemons/new
-  def new
-    # @pokemon = Pokemon.new
-  end
-
-  # GET /pokemons/1/edit
-  def edit
-  end
-
-  # POST /pokemons or /pokemons.json
-  def create
-  end
-
-  # PATCH/PUT /pokemons/1 or /pokemons/1.json
-  def update
-  end
-
-  # DELETE /pokemons/1 or /pokemons/1.json
-  def destroy
   end
 
   def results
@@ -48,15 +20,4 @@ class PokemonsController < ApplicationController
     last_modified_time = Time.strptime(timestamp, "%Y%m%d%H%M%S")
     fresh_when last_modified: last_modified_time, strong_etag: @pokemons
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pokemon
-      @pokemon = Pokemon.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def pokemon_params
-      params.require(:pokemon).permit(:name, :image_url)
-    end
 end
--- rails-version/app/views/pokemons/_pokemon.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! pokemon, :id, :created_at, :updated_at
-json.url pokemon_url(pokemon, format: :json)
