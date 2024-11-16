c8faef3291436ae0c44a7fd5a2d53255e352f9b0
Joseph Lozano <me@lozanojoseph.com>
2024-11-16 13:44:07 -0500

    fix image in results page

--- elixir-version/lib/roundest_phoenix/release.ex
+++ elixir-version/lib/roundest_phoenix/release.ex
@@ -11,6 +11,8 @@ defmodule RoundestPhoenix.Release do
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
+
+    RoundestPhoenix.GlobalSetup.run_seed()
   end
 
   def rollback(repo, version) do
